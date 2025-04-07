bbdd50f9ac9b2f109cc338d020df12e61932fdca
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2025-04-08 00:05:44 +0200

    config: add test config with jest

---
+++ jest.config.ts
@@ -0,0 +1,33 @@
+// filepath: /Users/edu/Projects/recipe-app/jest.config.ts
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.jest.json", // Usa el archivo específico para Jest
+      },
+    ],
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@babel|lodash-es|uuid|nanoid|pocketbase|next-toast-notify)/)",
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "app/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!**/test-utils/**",
+  ],
+  coverageReporters: ["text", "lcov"],
+};
+
+export default config;
