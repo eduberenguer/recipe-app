e5e2a0460afc468aa563f81bb15aeefb003024b6
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2025-04-18 22:50:06 +0200

    test: test in create recipe

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ Recipe App (WIP)
 ## Technologies
 
 - Next.js
-- PocketBase
-- Tailwind CSS
-- Cypress
 - TypeScript
-- React Context API
+- PocketBase
+- Jest and Supertest
+- Cypress
+- Tailwind CSS
+- React Context API, custom hooks and React Reducer API
 - Next.js Toast Notify / React Multi Carousel
 
 ## Features
@@ -32,10 +33,13 @@ Recipe App (WIP)
 - Recipe API (for food data)
 - Allergens Information
 - Weekly Menu
-- Backoffice
 - Pagination
 - Comments between users
 - Recipe Comments System
 - User-generated Recipe Collections
 - Multi-language Support
 - Dark Mode
+
+## <h3>📊 Test Coverage (work in progress)</h3>
+
+<img src="./public/images/githubImages/coverage.png" alt="Texto alternativo" width="300" height="600">
---
+++ app/api/recipes/create/create.test.ts
@@ -0,0 +1,90 @@
+import { POST } from "./route";
+import { createRecipe } from "@/server/recipes";
+import { mockRecipeWithIdv1 } from "@/app/__mocks__/recipe.mock";
+
+jest.mock("@/server/recipes", () => ({
+  createRecipe: jest.fn(),
+}));
+
+jest.mock("next/server", () => ({
+  NextResponse: {
+    json: jest.fn((data, { status = 200 } = {}) => ({
+      json: async () => data,
+      status,
+    })),
+  },
+}));
+
+function createFakeFormDataRequest(data: Record<string, string>): Request {
+  return {
+    formData: async () =>
+      ({
+        entries: () => Object.entries(data)[Symbol.iterator](),
+      } as unknown),
+  } as unknown as Request;
+}
+
+describe("POST /api/recipes/create", () => {
+  it("should return success and call create", async () => {
+    const fakeRequest = createFakeFormDataRequest({
+      title: "Test Recipe",
+      ingredients: JSON.stringify([{ name: "Salt", quantity: "1 tsp" }]),
+    });
+
+    (createRecipe as jest.Mock).mockResolvedValue({
+      ...mockRecipeWithIdv1,
+      success: true,
+    });
+
+    const response = await POST(fakeRequest);
+
+    expect(createRecipe).toHaveBeenCalledWith({
+      title: "Test Recipe",
+      ingredients: [{ name: "Salt", quantity: "1 tsp" }],
+    });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should return 400 ", async () => {
+    const fakeRequest = createFakeFormDataRequest({
+      title: "Test Recipe",
+      ingredients: JSON.stringify([{ name: "Salt", quantity: "1 tsp" }]),
+    });
+
+    (createRecipe as jest.Mock).mockResolvedValue({
+      error: "Unknown error",
+      success: false,
+    });
+
+    const response = await POST(fakeRequest);
+
+    expect(createRecipe).toHaveBeenCalledWith({
+      title: "Test Recipe",
+      ingredients: [{ name: "Salt", quantity: "1 tsp" }],
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 500 and unknown error", async () => {
+    const fakeRequest = createFakeFormDataRequest({
+      title: "Test Recipe",
+      ingredients: JSON.stringify([{ name: "Salt", quantity: "1 tsp" }]),
+    });
+
+    (createRecipe as jest.Mock).mockRejectedValue({
+      error: "Unknown error",
+      success: false,
+    });
+
+    const response = await POST(fakeRequest);
+
+    expect(createRecipe).toHaveBeenCalledWith({
+      title: "Test Recipe",
+      ingredients: [{ name: "Salt", quantity: "1 tsp" }],
+    });
+
+    expect(response.status).toBe(500);
+  });
+});
