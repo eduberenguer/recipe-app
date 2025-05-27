98fd2fadb78358ea2e59394c85ef769b059f2fb5
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2025-05-27 08:50:15 +0200

    feat: add new standar PATCH method

---
+++ app/api/recipes/toggleVisibleRecipe/[id]/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { updateRecipe } from "@/server/recipes";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = await params;
+    const { isVisible } = await req.json();
+    const result = await updateRecipe(id, { isVisible });
+
+    return NextResponse.json({ success: true, recipe: result });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
