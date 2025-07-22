7368745b07f73da3b8a4201aff2542819fa29969
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2025-07-22 08:23:34 +0200

    ref: add new mock in MessageAi

---
+++ app/__mocks__/mockInitialMessageAi.ts
@@ -0,0 +1,8 @@
+export const initialMessageAi = {
+  id: "init-ai-msg",
+  content:
+    "Hi! I'm your personal chef. What ingredients do you have, and how many people are you cooking for?",
+  fromUserId: "ai",
+  fromUserName: "ChefGPT",
+  created: new Date(),
+};
